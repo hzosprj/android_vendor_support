1456693541244241561fdbb67156b34d906a2db2
Anushek Prasal <anushekprasal@gmail.com>
2020-03-20 13:04:56 +0530

    support: Add and use SwitchPreference
    Change-Id: I55e47276a4f58e9b69241868ef17cb1d727b07bb

--- src/com/hz/support/preferences/GlobalSettingSwitchPreference.java
+++ src/com/hz/support/preferences/GlobalSettingSwitchPreference.java
@@ -18,7 +18,6 @@
 package com.hz.support.preferences;
 
 import android.content.Context;
-import androidx.preference.SwitchPreference;
 import android.util.AttributeSet;
 
 public class GlobalSettingSwitchPreference extends SwitchPreference {
